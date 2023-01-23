e4db2b54d2e59e738f5c181230fb80f4ad06cf13
Hal Evans <hal.evans@newtoneurope.com>
2023-01-23 17:01:17 +0000

    created ListContainer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import ListContainer from "./ListContainer";
 
 class App extends Component {
     render () {
         return(
             <>
             <h1>Ski App</h1>
+            <ListContainer />
             </>
         );
     }
---
+++ src/ListContainer.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import ListItem from "./ListItem";
+
+class ListContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            newItem: '',
+            taskCount: 0
+        }
+    }
+
+    addItemChange = (e) => {
+        this.setState({
+            newItem: e.target.value
+        })
+    }
+
+    addItem = (e) => {
+        e.preventDefault();
+
+        if (this.inputElement !== "") {
+            this.setState({
+                items: [...this.state.items, this.state.newItem],
+                newItem: ''
+            })
+        }
+    }
+
+    render() {
+
+        const allItems = this.state.items.map((item, index) => {
+            return <ListItem />
+        });
+
+        return(
+            <>
+                <form>
+                    <input
+                        className="input"
+                        placeholder="Add ski resort..."
+                        onChange={this.addItemChange}
+                        value={this.state.newItem} />
+                    <button
+                        className="submit-button"
+                        type="submit"
+                        onClick={this.addItem}>Submit</button>
+                </form>
+
+                <ul>
+                    {allItems}
+                </ul>
+            </>
+        );
+    }
+}
+
+export default ListContainer;
