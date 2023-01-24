129b66a5f1020d35c411d8ff89c1f0b56c1016e6
Hal Evans <hal.evans@newtoneurope.com>
2023-01-24 13:36:32 +0000

    ListContainer handleChange method added

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -4,18 +4,27 @@ import ListInput from "./ListInput";
 
 class ListContainer extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state={
             items: [],
             id:0,
             item: ''
         }
-    } 
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            item: e.target.value
+        });
+    }
+
     render() {
         return(
             <>
                 <h2>ListContainer</h2>
-                <ListInput />
+                <ListInput 
+                    item={this.state.item}
+                    handleChange={this.handleChange} />
                 <ItemsList />
             </>
         );
