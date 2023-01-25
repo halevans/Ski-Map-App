94fa9dd67fd32c899ada4eccc0543c2d2cbefab3
Hal Evans <hal.evans@newtoneurope.com>
2023-01-25 10:59:27 +0000

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import ListContainer from "./ListContainer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
     render () {
         return(
             <>
             <h1>Ski App</h1>
-            <ListContainer />
+            <div className="container">
+                <ListContainer />
+            </div>
             </>
         );
     }
